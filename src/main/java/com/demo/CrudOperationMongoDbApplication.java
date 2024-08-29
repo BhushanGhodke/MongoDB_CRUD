ecbee2a66da4a579a63140b2589df949f9cb8157
BhushanGhodke <bhushanghodke1500@gmail.com>
2024-08-29 20:35:47 +0530

    MongoDB CRUD Project Repository Created

---
+++ src/main/java/com/demo/CrudOperationMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationMongoDbApplication.class, args);
+	}
+
+}
